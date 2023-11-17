28f3de1d008a88f22408806aa7ed1cfcb9273555
Tiitus <tiitus.laine@edu.hyria.fi>
2023-11-17 10:19:20 +0200

    Add project files.

---
+++ AlgoVisu/Program.cs
@@ -0,0 +1,59 @@
+﻿using AlgoVisu;
+using System.Linq;
+using Raylib_cs;
+
+namespace HelloWorld;
+
+class Program
+{
+    private static int[] genArray()
+    {
+        int[] values = new int[10000];
+        
+        for (int i = 0; i < values.Length; i++)
+        {
+            values[i] = i;
+        }
+
+        var rng = new Random();
+        var shuffledArray = values.OrderBy(e => rng.NextDouble()).ToArray();
+
+        /*foreach (int i in shuffledArray)
+        {
+            Console.WriteLine(i);
+        }*/
+
+        return shuffledArray;
+    }
+
+    public static void Main()
+    {
+        algos sort = new algos();
+        int[] values = genArray();
+        sort.bubble(values);
+       
+        foreach (int i in values)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+
+
+
+
+/*'
+Raylib.InitWindow(800, 480, "Hello World");
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.WHITE);
+    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+
+
+    Raylib.EndDrawing();;
+}
+
+Raylib.CloseWindow();
+*/
---
+++ AlgoVisu/algos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoVisu
+{
+    internal class algos
+    {
+        public void bubble(int[] values)
+        {
+            bool swapped = true;
+            int n = values.Length;
+
+            // Repeat while values are being swapped
+            // Sorts from smallest to biggest
+            while (swapped){
+                swapped = false;
+                for (int i = 0; i < n -1; i++)
+                {
+                    if (values[i] > values[i + 1])
+                    {
+                        swapped = true;
+                        int placeholder = values[i + 1];
+                        values[i + 1] = values[i];
+                        values[i] = placeholder;
+                    }
+                }
+                n = n - 1;
+            }
+        }
+        public void 
+    }
+}
